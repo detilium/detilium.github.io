b9b38ae77b8dec3beceaedb4df15863dc9fdf925
Christian Haase <chrhaase@icloud.com>
2026-02-14 19:49:51 +0100

    base layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://detilium.github.io'
+  site: 'https://detilium.github.io',
+  vite: { plugins: [tailwindcss()] }
 });
